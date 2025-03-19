df56ccf9e176a47ae788930da29072c3ea43c1bf
Tykosmedborn <tykosmedborn@gmail.com>
2025-03-19 21:42:11 +0100

    Add files via upload

---
+++ Insanity_sort.ts
@@ -0,0 +1,35 @@
+
+// Sorts an array by reversing the array and hoping something is different this time
+function insanity_sort(array: Array<any>): Array<any> {
+    if (help_function(array) === true) {
+        return array;
+    }  
+    return insanity_sort(array.reverse());
+}
+
+//Checks if function is sorted from smallest to largest
+function help_function(array: Array<any>) {
+    let current: number = 0;
+    if (array.length <= 1) {
+        return true;
+    }
+    while (current < array.length ) {
+        if (array[current] > array[current + 1]) {
+            return false;
+        }
+        current = current + 1;
+    }
+    true
+}
+//I am not crazy! I know he swapped those numbers. I knew it was 1216. One after Magna Carta. As if I could ever make such a mistake. Never. Never! I just – I just couldn’t prove it.
+// He covered his tracks, he got that idiot at the copy shop to lie for him. You think this is something? You think this is bad? This? This chicanery? He’s done worse.
+// That billboard! Are you telling me that a man just happens to fall like that? No! He orchestrated it! Jimmy! He defecated through a sunroof! And I saved him! And I shouldn’t have.
+// I took him into my own firm! What was I thinking? He’ll never change. He’ll never change! Ever since he was 9, always the same! Couldn’t keep his hands out of the cash drawer!
+// But not our Jimmy! Couldn’t be precious Jimmy! Stealing them blind! And HE gets to be a lawyer? What a sick joke! I should’ve stopped him when I had the chance!
+// …And you, you have to stop him! You-
+
+
+//Did I ever tell you what the definition of insanity is? Insanity is doing the exact... same fucking thing... over and over again expecting... shit to change... That. Is. Crazy. 
+// The first time somebody told me that, I dunno, I thought they were bullshitting me, so, I shot him. The thing is... He was right. 
+// And then I started seeing, everywhere I looked, everywhere I looked all these fucking pricks, everywhere I looked, doing the exact same fucking thing... 
+// over and over and over and over again thinking 'this time is gonna be different' no, no, no please... This time is gonna be different, I'm sorry, I don't like... The way...
